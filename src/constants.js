756a568042ea66ab20e352d9c77cb1adcc520d3b
lauragreen <lollymay@gmail.com>
2019-04-09 12:52:25 +0100

    Add dotenv to keep token secret

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
-export const APP_PORT = 5000
-export const BASE_URL = 'https://swapi.co/api' // star wars API base URL
+require('dotenv').config({debug: process.env.DEBUG});
+export const APP_PORT = 5000;
+// export const BASE_URL = 'https://swapi.co/api' // star wars API base URL
+export const BASE_URL = 'https://trefle.io/api/'; // Trefle API base url
+export const TOKEN = process.env.LEAF;
